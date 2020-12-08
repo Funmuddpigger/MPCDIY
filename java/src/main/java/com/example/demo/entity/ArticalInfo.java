cd19ac6becad8c2acdf9b64977cbe435bb3b21c6
Funmuddpigger <tb963472217@aol.com>
2020-12-08 11:51:05 +0800

    MPC JAVA

---
+++ java/src/main/java/com/example/demo/entity/ArticalInfo.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+
+public class ArticalInfo implements Serializable {
+
+    private Integer id;
+
+    private Integer articalId;
+
+    private String articalText;
+
+    private String articalType;
+
+    private String articalTitle;
+
+    private String articalComment;
+
+    private Integer authorId;
+
+    private String authorName;
+
+    private static final long serialVersionUID = 1L;
+
+
+}
---
+++ java/src/main/java/com/example/demo/mapper/ArticalInfoMapper.java
@@ -0,0 +1,10 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.ArticalInfo;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ArticalInfoMapper {
+
+}
